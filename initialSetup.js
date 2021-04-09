5d1e41b1794c9b5f1e9b5fbf1c8ad6dfadb48872
likhithst <tolikhith@gmail.com>
2021-04-09 16:36:10 +0530

    commit

--- initialSetup.js
+++ initialSetup.js
@@ -6,7 +6,7 @@ const mongodb = require("mongodb")
 mongodb.connect(process.env.CONNECTIONSTRING,{useNewUrlParser:true,useUnifiedTopology:true},function (err, client) {
     module.exports = client;
     const app = require('./index');
-    app.listen(3000,function () {
+    app.listen(process.env.PORT||3000,function () {
         console.log('code running at port 3000');
     })
 })
